3aed57937a3cd823911442db34cde80efa90ad73
Amin-Ahmidout <AhmidoutA@hotmail.com>
2024-02-21 15:16:31 +0100

    CursistID weggehaald uit CursistController

--- src/cursist/CursistController.java
+++ src/cursist/CursistController.java
@@ -95,7 +95,6 @@ public class CursistController {
                     cursist.setAddress(rs.getString("Address"));
                     cursist.setCity(rs.getString("City"));
                     cursist.setCountry(rs.getString("Country"));
-                    cursist.setcursistID(rs.getInt("CursistID"));
 
                     return cursist;
                 }
